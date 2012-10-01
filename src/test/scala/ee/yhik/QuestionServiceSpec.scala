64419e4da8a03ba3fa0fc536c11fd5ef37e5429c
anton <anton@home>
2012-10-01 23:49:16 +0300

    Root sends redirect.

--- src/test/scala/ee/yhik/QuestionServiceSpec.scala
+++ src/test/scala/ee/yhik/QuestionServiceSpec.scala
@@ -3,14 +3,15 @@ package ee.yhik
 import org.scalatra.test.specs2._
 
 // For more on Specs2, see http://etorreborre.github.com/specs2/guide/org.specs2.guide.QuickStart.html 
-class QuestionServiceSpec extends ScalatraSpec { def is =
-  "GET / on QuestionService"                     ^
-    "should return status 200"                  ! root200^
-                                                end
-    
+class QuestionServiceSpec extends ScalatraSpec {
+  def is =
+    "GET / on QuestionService" ^
+      "should return redirect" ! root302 ^
+      end
+
   addServlet(classOf[QuestionService], "/*")
 
-  def root200 = get("/") { 
-    status must_== 200
+  def root302 = get("/") {
+    status must_== 302
   }
 }
